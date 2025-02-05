beb83888742398e3e89191fdc5bc78a505ec26db
jacksontecn <arakisu@gmail.com>
2025-02-05 16:33:38 -0300

    First driver earnings project committee

---
+++ src/main/java/com/mowtechnologies/earnings_driving/EarningsDrivingApplication.java
@@ -0,0 +1,13 @@
+package com.mowtechnologies.earnings_driving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarningsDrivingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarningsDrivingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mowtechnologies/earnings_driving/EarningsDrivingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mowtechnologies.earnings_driving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarningsDrivingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
